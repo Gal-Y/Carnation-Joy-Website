acda24a1dcd0d453bdf7b796a2b5747cfa9fcd0b
Galen Yuan <galenYuan@student.unsw.edu.au>
2025-02-10 12:20:31 +1100

    updated API end point for carer

--- js/become.js
+++ js/become.js
@@ -37,7 +37,7 @@ document.getElementById("carer-form").addEventListener("submit", async function 
     submitButton.innerText = "Submitting...";
 
     // API Gateway Endpoint (Replace with your actual AWS API Gateway URL)
-    const apiEndpoint = "https://your-api-id.execute-api.region.amazonaws.com/prod/become-carer";
+    const apiEndpoint = "https://6l20ufzu4d.execute-api.ap-southeast-2.amazonaws.com/prod";
 
     try {
         const response = await fetch(apiEndpoint, {
